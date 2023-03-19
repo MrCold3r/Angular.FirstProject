9a6e05711c898fa806435291e7d35b8435ee255d
MrCold3r <mr.colder13@gmail.com>
2023-03-20 02:25:49 +0400

    Bootsrap Modal, NavBar, Footer

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,12 +4,22 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+
+import { NavbarComponent } from './navbar/navbar.component';
+import { FooterComponent } from './footer/footer.component';
+import { SectionComponent } from './section/section.component';
+
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    FooterComponent,
+    SectionComponent
   ],
   imports: [
     BrowserModule,
+    NgbModule,
     AppRoutingModule
   ],
   providers: [],
